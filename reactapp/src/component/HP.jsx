f8b696107b50c6cf0fbccfde22b1e58db5a00100
vamanverma161 <vamanverma161@gmail.com>
2025-01-28 22:15:02 +0530

    Home page for reactapp

---
+++ reactapp/src/component/HP.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HP() {
+  return (
+    <div className='container'>
+      <div className="first">
+        <img src="https://static1.makeuseofimages.com/wp-content/uploads/2014/09/tech-change-world.jpg" alt="" />
+      </div>
+      <div className="sec">
+        
+      </div>
+    </div>
+  )
+}
+
+export default HP
---
+++ reactapp/src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <img src="https://static-00.iconduck.com/assets.00/react-icon-512x456-5xl7nmtw.png" alt="logo" />
+        <h2>React-App</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
